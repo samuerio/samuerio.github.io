de0d06f21d7da9ba15df49c51d08e2fc3885dd15
郑贺 <853713419@qq.com>
2016-11-20 17:02:43 +0800

    Site updated: 2016-11-20 17:02:42

--- js/jquery-spyNavMenu.js
+++ js/jquery-spyNavMenu.js
@@ -4,10 +4,25 @@
  *
  *  Author: zhenghe39@gmail.com
  *  Usage:
+ *
+ *      var navMenu = $(element).spyNavMenu({
+ *          scrollNav:function(){},
+ *          activeClass:'',
+ *          fixOffsetClass:'',
+ *          distinguishLineScale:''
+ *      });
+ *      navMenu.on('scrollNav',callBack(params...){})
+ *
+ *
  *      事件:
      *      scrollNav,
-     *      CallBack($curNavItem{当前激活的目录项元素},$navMenu{目录元素},curNavItemHeight{当前激活的目录项元素的高度},curNavItemTop{当前激活的目录项元素距离父元素顶端的距离})
-     *      Note:由于在JQuery中,当父元素被隐藏,那么子元素的height()的值就会变为0,故保存NavItem高度curNavItemHeight. curNavItemTop也是同理
+     *      CallBack(navItem{当前激活的目录项实体对象},$navMenu{目录元素});
+     *      navItem = {
+     *          $ele:当前激活的目录项元素,
+     *          height:当前激活的目录项元素的高度,
+     *          top:当前激活的目录项元素距离父元素顶端的距离
+     *      }
+     *      Note:由于在JQuery中,当父元素被隐藏,那么子元素的height()的值就会变为0,故保存$navItem高度height. top也是同理
  *
  *      自定义配置属性:
  *          activeClass:激活的目录项元素Class,默认值为'active'
@@ -22,29 +37,36 @@
  *                 eg.1/2代表当前窗口的中间线作为基准线).默认值为0,即代表以窗口顶端的线作为基准线
  *
  *
+ *      其他:
+ *          使用插件后,会返回navMenu实体对象.
+ *          或者也可以通过$(element).data('entity')获取该实体对象.
+ *
+ *
  */
 
 +(function($){
     $.extend($.fn,{
         spyNavMenu:function(options){
             var navMenu = new $.NavMenu(options,this[0]);
+            if(this.data('entity')){
+                this.data('entity',navMenu);
+            }
             return navMenu;
         }
     });
 
     //NavMenu Creator
-    $.NavMenu = function(options,navEle){
-        this.settings = $.extend({}, $.NavMenu.defaults,options);
+    $.NavMenu = function(options,navMenuEle){
+        //Note:用户自定义事件
         this.eventLoop =$.extend({}, options.events);
         delete options.events;
-        this.navEle = navEle;
-        this.init();
+
+        this.settings = $.extend({}, $.NavMenu.defaults,options);
+        this.navMenuEle = navMenuEle;
+        this._init();
     }
 
     $.extend($.NavMenu,{
-        Events:{
-            scrollNav:'scrollNav'
-        },
         defaults:{
             activeClass:'active',
             fixOffsetClass:'nav-fix',
@@ -52,14 +74,18 @@
             events:{}
         },
         prototype:{
-            init:function(){
-                var _self = this
-                var navEle = _self.navEle;
+            _init:function(){
+                var _self = this;
+                var navMenuEle = _self.navMenuEle;
 
-                _self._navSectionInfos = _self._getNavSectionInfos(navEle);
-                _self._navTopOffset = $(_self.navEle).offset().top;
-                _self._listener().scrollEvent();
+                _self.properties = {
+                    navMenuTopOffset: $(navMenuEle).offset().top,
+                    navItemSections: _self._getNavItemSections(navMenuEle)
+                }
+
                 $(window).on('scroll',_self._listener().scrollEvent)
+                //初始化当前的NavItemEle,进行高亮
+                _self._listener().scrollEvent();
             },
             /**
              * Event listeners
@@ -74,32 +100,33 @@
                      * @param event
                      */
                     scrollEvent:function(event){
-                        var navTopOffset = _self._navTopOffset;
-                        var scrollDistance = $(this).scrollTop();
 
                         //Note: Nav fixed
-                        var navEle = _self.navEle;
+                        var navMenuEle = _self.navMenuEle;
                         var fixOffsetClass = _self.settings.fixOffsetClass;
-                        if(scrollDistance >= navTopOffset){
-                            $(navEle).addClass(fixOffsetClass);
+                        if($(window).scrollTop() >= _self.properties.navMenuTopOffset){
+                            $(navMenuEle).addClass(fixOffsetClass);
                         }else{
-                            $(navEle).removeClass(fixOffsetClass);
+                            $(navMenuEle).removeClass(fixOffsetClass);
                         }
 
                         //Note: distinguish line
                         //默认基准判别线为0*windowHeight,即Window顶端
-                        var distinguishLineOffset = _self.settings.distinguishLineScale*$(window).height()+$(window).scrollTop();
+                        var distinguishLineOffset =
+                            _self.settings.distinguishLineScale*$(window).height()+$(window).scrollTop();
 
+                        //Note: remove all activeClass before focus curNavItem
                         var activeClass = _self.settings.activeClass;
-                        $(_self.navEle).find('.'+activeClass).removeClass(activeClass)
-                        $('.highlight-title').hide();
+                        $(_self.navMenuEle).find('.'+activeClass)
+                            .removeClass(activeClass);
 
-                        var curNavSectionInfo = _self._getCurNavSectionInfo(_self._navSectionInfos,distinguishLineOffset);
-                        curNavSectionInfo && curNavSectionInfo.$navMenuItem.addClass(activeClass);
+                        var curNavItemSection = _self._getCurNavSection(_self.properties.navItemSections,distinguishLineOffset);
+                        curNavItemSection && curNavItemSection.navItem.$ele.addClass(activeClass);
+
                         //执行用户自定义scroolNav事件
-                        _self.eventLoop[$.NavMenu.Events.scrollNav] &&
-                                    _self.eventLoop[$.NavMenu.Events.scrollNav](curNavSectionInfo && curNavSectionInfo.$navMenuItem,$(_self.navEle),
-                                        curNavSectionInfo && curNavSectionInfo.navMenuItemHeight,curNavSectionInfo && curNavSectionInfo.navMenuItemTop)
+                        var eventLoop = _self.eventLoop;
+                        eventLoop['scrollNav'] &&
+                                    eventLoop['scrollNav'](curNavItemSection && curNavItemSection.navItem,$(_self.navMenuEle))
 
                     }
                 }
@@ -110,51 +137,57 @@
              * @returns {Array}
              * @private
              */
-            _getNavSectionInfos:function(navEle){
-                var navSectionInfo = [];
+            _getNavItemSections:function(navMenuEle){
+                var navItemSections = [];
 
-                $(navEle).find('a').each(function(){
+                $(navMenuEle).find('a').each(function(){
+                    var _self = this;
+                    var $navItem = $(_self).parent();
                     var sectionId = $(this).attr('href').split('#')[1];
 
-                    navSectionInfo.push({
+                    navItemSections.push({
                         id:sectionId,
                         offsetTop:$('#'+sectionId).offset().top,
-                        $navMenuItem:$(this).parent(),
-                        navMenuItemHeight:$(this).parent().height(),
-                        navMenuItemTop:$(this).parent().position().top
+                        navItem:{
+                            $ele:$navItem,
+                            height:$navItem.height(),
+                            top:$navItem.position().top
+                        }
                     });
                 });
 
-                return navSectionInfo;
+                return navItemSections;
             },
             /**
-             * Get Current NavSectionInfo
-             * @param navSectionInfos
+             * Get Current navItemSection
+             * @param navItemSections
              * @param distinguishLineOffset
              * @returns {*}
              * @private
              */
-            _getCurNavSectionInfo:function(navSectionInfos,distinguishLineOffset){
-                var curNavSectionInfo = null;
+            _getCurNavSection:function(navItemSections,distinguishLineOffset){
+                var curNavItemSection = null;
 
-                var navSectionCount = navSectionInfos && navSectionInfos.length;
-                if(navSectionCount){
-                    if(1 === navSectionCount){
-                        if(distinguishLineOffset >= navSectionInfos[0].offsetTop){
-                            curNavSectionInfo = navSectionInfos[0];
+                var count = navItemSections && navItemSections.length;
+                if(count){
+                    if(1 === count){
+                        if(distinguishLineOffset >= navItemSections[0].offsetTop){
+                            curNavItemSection = navItemSections[0];
                         }
                     }else{
-                        $.each(navSectionInfos,function(idx,navSectionInfo){
-                            if(idx !== navSectionCount-1){
-                                var nextNavSectionInfo = navSectionInfos[idx+1];
-                                if(navSectionInfo.offsetTop<= distinguishLineOffset
+                        $.each(navItemSections,function(idx,navItemSection){
+                            if(idx !== count-1){//非最后一个navSection
+                                //当distinguishLineOffset 位于第n个navItemSection的后面 && n+1个navItemSection的前面时,
+                                // 这第n个navItemSection所对应的navItem为当前目录导航项,需要进行高亮
+                                var nextNavSectionInfo = navItemSections[idx+1];
+                                if(navItemSection.offsetTop <= distinguishLineOffset
                                     && distinguishLineOffset < nextNavSectionInfo.offsetTop){
-                                    curNavSectionInfo = navSectionInfo;
+                                    curNavItemSection = navItemSection;
                                     return false;
                                 }
-                            }else{
-                                if(navSectionInfo.offsetTop<= distinguishLineOffset){
-                                    curNavSectionInfo = navSectionInfo;
+                            }else{//最后一个navSection
+                                if(navItemSection.offsetTop <= distinguishLineOffset){
+                                    curNavItemSection = navItemSection;
                                     return false;
                                 }
                             }
@@ -162,7 +195,7 @@
                     }
                 }
 
-                return curNavSectionInfo;
+                return curNavItemSection;
             },
             /**
              * 对外提供的事件绑定入口
